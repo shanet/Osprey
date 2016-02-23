ec73380c88bef89577c49212d9673b6cdff3e437
shane tully <shane@shanetully.com>
2016-02-22 23:31:04 -0800

    add empty android project

---
+++ android/src/androidTest/java/com/shanet/osprey/MainTest.java
@@ -0,0 +1,21 @@
+package com.shanet.osprey;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * This is a simple framework for a test of an Application.  See
+ * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
+ * how to write and extend Application tests.
+ * <p/>
+ * To run this test, you can type:
+ * adb shell am instrument -w \
+ * -e class com.shanet.osprey.MainTest \
+ * com.shanet.osprey.tests/android.test.InstrumentationTestRunner
+ */
+public class MainTest extends ActivityInstrumentationTestCase2<Main> {
+
+    public MainTest() {
+        super("com.shanet.osprey", Main.class);
+    }
+
+}

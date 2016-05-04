e54e19d969228377863206c5e5e52b0d676a057a
shane tully <shane@shanetully.com>
2016-05-03 23:19:28 -0700

    make tracking UI pretty

--- android/app/src/main/java/com/shanet/osprey/TrackingFragment.java
+++ android/app/src/main/java/com/shanet/osprey/TrackingFragment.java
@@ -26,36 +26,35 @@ import android.widget.TextView;
 public class TrackingFragment extends DatasetFragment implements LocationListener, SensorEventListener {
   private static final int EARTH_RADIUS = 6371009;
 
-  private double rocketLatitude;
-  private double rocketLongitude;
-  private double userLatitude;
-  private double userLongitude;
+  private Double rocketLatitude;
+  private Double rocketLongitude;
+  private double deviceLatitude;
+  private double deviceLongitude;
+
+  private float distance;
   private float heading;
+  private float relativeBearing;
 
   private Sensor orientationSensor;
   private SensorManager sensorManager;
 
   private ImageView arrowDisplay;
-  private TextView rocketCoordinatesDisplay;
-  private TextView userCoordinatesDisplay;
-  private TextView distanceDisplay;
-  private TextView relativeBearingDisplay;
-  private TextView headingDisplay;
-  private TextView magneticBearingDisplay;
+  private ImageView compassDisplay;
+  private TextView vectorDisplay;
 
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     View layout = inflater.inflate(R.layout.tracking_fragment, null);
 
+    rocketLatitude = null;
+    rocketLongitude = null;
+
+    distance = 0;
     heading = 0;
+    relativeBearing = 0;
 
     arrowDisplay = (ImageView)layout.findViewById(R.id.arrow);
-    rocketCoordinatesDisplay = (TextView)layout.findViewById(R.id.rocket_coordinates_display);
-    userCoordinatesDisplay = (TextView)layout.findViewById(R.id.user_coordinates_display);
-    distanceDisplay = (TextView)layout.findViewById(R.id.distance_display);
-
-    relativeBearingDisplay = (TextView)layout.findViewById(R.id.relative_bearing_display);
-    headingDisplay = (TextView)layout.findViewById(R.id.heading_display);
-    magneticBearingDisplay = (TextView)layout.findViewById(R.id.magnetic_bearing_display);
+    compassDisplay = (ImageView)layout.findViewById(R.id.compass);
+    vectorDisplay = (TextView)layout.findViewById(R.id.vector_display);
 
     LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
     locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
@@ -80,20 +79,8 @@ public class TrackingFragment extends DatasetFragment implements LocationListene
     // Don't update if not added to an activity yet
     if(!isAdded()) return;
 
-    // Update the rocket coordinates label
-    String coordinates = dataset.getCoordinates();
-    updateDisplay(rocketCoordinatesDisplay, coordinates, R.string.default_coordinates, R.string.rocket_coordinates);
-
-    Double latitude = (Double)dataset.getField("latitude");
-    Double longitude = (Double)dataset.getField("longitude");
-
-    // Only update the rocket position if coordinates exist
-    if(latitude != null && longitude != null) {
-      rocketLatitude = latitude.doubleValue();
-      rocketLongitude = longitude.doubleValue();
-
-      updateDistance();
-    }
+    rocketLatitude = (Double)dataset.getField("latitude");
+    rocketLongitude = (Double)dataset.getField("longitude");
   }
 
   public String getTitle(Context context) {
@@ -106,14 +93,15 @@ public class TrackingFragment extends DatasetFragment implements LocationListene
     // Don't update if not added to an activity yet
     if(!isAdded()) return;
 
-    userLatitude = location.getLatitude();
-    userLongitude = location.getLongitude();
+    deviceLatitude = location.getLatitude();
+    deviceLongitude = location.getLongitude();
 
-    // Update the user coordinates label
-    updateDisplay(userCoordinatesDisplay, String.format("%f, %f", userLatitude, userLongitude), 0, R.string.user_coordinates);
-
-    updateDistance();
-    updateBearing();
+    if(rocketLatitude != null && rocketLongitude != null) {
+      updateDistance();
+      updateRelativeBearing();
+      updateVector();
+      updateImage(arrowDisplay, relativeBearing);
+    }
   }
 
   public void onProviderEnabled(String provider) {}
@@ -125,53 +113,67 @@ public class TrackingFragment extends DatasetFragment implements LocationListene
   // ---------------------------------------------------------------------------------------------------
   public void onSensorChanged(SensorEvent event) {
     heading = event.values[0];
-    updateBearing();
+
+    updateImage(compassDisplay, 360f - (float)heading);
+
+    if(rocketLatitude != null && rocketLongitude != null) {
+      updateRelativeBearing();
+      updateVector();
+      updateImage(arrowDisplay, relativeBearing);
+    }
   }
 
   public void onAccuracyChanged(Sensor sensor, int accuracy) {}
   // ---------------------------------------------------------------------------------------------------
 
   private void updateDistance() {
+    double rocketLatitude = this.rocketLatitude.doubleValue();
+    double rocketLongitude = this.rocketLongitude.doubleValue();
+
     // https://en.wikipedia.org/wiki/Haversine_formula
-    double deltaLatitude = Math.toRadians(userLatitude - rocketLatitude);
-    double deltaLongitude = Math.toRadians(userLongitude - rocketLongitude);
+    double deltaLatitude = Math.toRadians(deviceLatitude - rocketLatitude);
+    double deltaLongitude = Math.toRadians(deviceLongitude - rocketLongitude);
 
     double sinDeltaLatitude = Math.sin(deltaLatitude / 2);
     double sinDeltaLongitude = Math.sin(deltaLongitude / 2);
 
     double intermediate = Math.pow(sinDeltaLatitude, 2)
       + Math.cos(Math.toRadians(rocketLatitude))
-      * Math.cos(Math.toRadians(userLatitude))
+      * Math.cos(Math.toRadians(deviceLatitude))
       * Math.pow(sinDeltaLongitude, 2);
 
-    double distance = 2 * EARTH_RADIUS * Math.asin(Math.sqrt(intermediate));
-
-    updateDisplay(distanceDisplay, String.format("%.2f", distance), 0, R.string.distance, R.string.meters);
+    distance = (float)(2 * EARTH_RADIUS * Math.asin(Math.sqrt(intermediate)));
   }
 
-  private void updateBearing() {
+  private void updateRelativeBearing() {
     // Calculate the magnetic bearing from the rocket's location to magnetic north
-    double deltaLongitude = Math.toRadians(rocketLongitude - userLongitude);
+    double deltaLongitude = Math.toRadians(rocketLongitude - deviceLongitude);
 
     double y = Math.sin(deltaLongitude) * Math.cos(Math.toRadians(rocketLatitude));
-    double x = Math.cos(Math.toRadians(userLatitude)) * Math.sin(Math.toRadians(rocketLatitude)) -
-      Math.sin(Math.toRadians(userLatitude)) * Math.cos(Math.toRadians(rocketLatitude)) * Math.cos(deltaLongitude);
+    double x = Math.cos(Math.toRadians(deviceLatitude)) * Math.sin(Math.toRadians(rocketLatitude)) -
+      Math.sin(Math.toRadians(deviceLatitude)) * Math.cos(Math.toRadians(rocketLatitude)) * Math.cos(deltaLongitude);
 
     double magneticBearing = Math.toDegrees(Math.atan2(y, x));
     magneticBearing = (magneticBearing + 360) % 360;
 
     // RB = MB - MH
-    double relativeBearing = magneticBearing - heading;
+    relativeBearing = (float)magneticBearing - heading;
+  }
 
-    updateDisplay(relativeBearingDisplay, String.format("%.0f", relativeBearing), 0, R.string.relative_bearing, R.string.degrees);
-    updateDisplay(headingDisplay, String.format("%.0f", heading), 0, R.string.heading, R.string.degrees);
-    updateDisplay(magneticBearingDisplay, String.format("%.0f", magneticBearing), 0, R.string.magnetic_bearing, R.string.degrees);
+  private void updateVector() {
+    String vector = String.format("%.2f%s %s %.0f%s", distance, getString(R.string.meters), getString(R.string.at), relativeBearing, getString(R.string.degrees));
+    updateDisplay(vectorDisplay, vector, 0);
+  }
 
-    // Rotate the arrow
+  private void updateImage(ImageView image, float angle) {
+    // Rotate the arrow to the calculated bearing
     Matrix matrix = new Matrix();
-    matrix.setRotate((float)relativeBearing, arrowDisplay.getDrawable().getBounds().width()/2, arrowDisplay.getDrawable().getBounds().height()/2);
 
-    arrowDisplay.setScaleType(ImageView.ScaleType.MATRIX);
-    arrowDisplay.setImageMatrix(matrix);
+    float width = image.getDrawable().getBounds().width();
+    float height = image.getDrawable().getBounds().height();
+    matrix.setRotate(angle, width/2, height/2);
+
+    image.setScaleType(ImageView.ScaleType.MATRIX);
+    image.setImageMatrix(matrix);
   }
 }
